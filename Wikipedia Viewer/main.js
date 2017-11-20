6ccf7735f3a0309a455bfbde80dbb8d124bd4c05
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2017-11-20 21:36:47 +0530

    Add wiki viewer project

--- Wikipedia Viewer/main.js
+++ Wikipedia Viewer/main.js
@@ -1,19 +1,138 @@
 $(document).ready(function () {
-    var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cpageviews&list=search&utf8=1&srsearch=Albert%20Einstein";
 
-    $.ajax({
-        url: url,
-        type: 'GET',
-        crossDomain: true,
-        headers: {
-            'Api-User-Agent': 'WikipediaViewer/1.0',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        xhrFields: {
-            withCredentials: false
-        },
-        success: function (data) {
-            console.log(data);
+    $loader = $('#loader');
+    $clear_search = $('#clear-search');
+    $link_random_article = $('#link-random-article');
+    $search_result_container = $('#search-result-container');
+    $search_input = $('#search-input');
+    $search_results = $("#search-results");
+
+    const IDLE = 0;
+    const LOADING = 1;
+    const RESULT = 2;
+
+    var setViewState = function (state) {
+        switch (state) {
+            case IDLE:
+                $loader.fadeOut(150);
+                $loader.addClass('hide');
+
+                $clear_search.fadeOut(100);
+                $clear_search.addClass('hide');
+
+                $link_random_article.fadeIn(100);
+                $link_random_article.removeClass('hide');
+
+                $search_result_container.fadeOut(100);
+                break;
+            case LOADING:
+                $link_random_article.fadeOut(150, function () {
+                    $loader.removeClass('hide');
+                    $loader.fadeIn(150);
+                });
+                $link_random_article.addClass('hide');
+
+                $clear_search.fadeOut(100);
+                $clear_search.addClass('hide');
+
+                $search_result_container.fadeOut(100);
+                $search_result_container.addClass('hide');
+                break;
+            case RESULT:
+                $loader.fadeOut(150);
+                $loader.addClass('hide');
+
+                $clear_search.removeClass('hide');
+                $clear_search.fadeIn(100);
+
+                $link_random_article.fadeOut(100);
+                $link_random_article.addClass('hide');
+
+                $search_result_container.fadeIn(100);
+                $search_result_container.removeClass('hide');
+                break;
         }
+    };
+
+    $search_input.val('');
+    setViewState(IDLE);
+
+    var searchString = function () {
+        var string = $search_input.val();
+        setViewState(LOADING);
+        var url = "https://en.wikipedia.org/w/api.php?origin=*";
+
+        dataobj = {
+            action: 'query',
+            format: 'json',
+            prop: ['info', 'pageviews', 'fullurl'],
+            list: 'search',
+            srsearch: encodeURI(string)
+        };
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            crossDomain: true,
+            data: dataobj,
+            dataType: 'json',
+            headers: {
+                'Api-User-Agent': 'WikipediaViewer/1.1 (http://rajanikant.me/WikipediaViewer/; WikipediaViewer@rajanikant.me)'
+            },
+            success: function (data) {
+
+                setViewState(RESULT);
+
+                var resultArr = data.query.search;
+                $search_results.empty();
+
+                if (resultArr.length > 0) {
+                    for (var i = 0; i < resultArr.length; i++) {
+                        var obj = resultArr[i];
+                        var title = obj['title'];
+                        console.log(obj);
+                        var snippet = obj['snippet'];
+                        $search_results.append(['<li class="collection-item">',
+                            '<a target="_blank" href="', 'https://en.wikipedia.org/wiki/', encodeURI(title), '">',
+                            '<div class="result-card">',
+                            '<div class="result-title">', title, '</div>',
+                            '<div class="result-snippet">', snippet, '</div>',
+                            '</div>',
+                            '</a>',
+                            '</li>'].join(''));
+                    }
+                }else {
+                    $search_results.append(['<li class="collection-item">',
+                        '<div class="result-card-empty center-align">',
+                        '<div class="result-title">', 'No results found.', '</div>',
+                        '</div>',
+                        '</li>'].join(''));
+                }
+            }
+        });
+    };
+
+    $search_input.on('change', function () {
+        searchString();
+    });
+
+    $('#search-icon').on('click', function () {
+        if ($search_input.val() !== '') {
+            searchString()
+        }
+    });
+
+    $search_input.on('input', function () {
+        if ($(this).val() !== '') {
+            $('#search-icon').addClass('clickable-search-icon btn-flat');
+
+        } else {
+            $('#search-icon').removeClass('clickable-search-icon btn-flat');
+        }
+    });
+
+    $clear_search.on('click', function () {
+        $search_input.val('');
+        setViewState(IDLE)
     });
 });
